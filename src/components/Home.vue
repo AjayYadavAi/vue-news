da7bcb9969ab7b092d722962f294d8fbf6696ebc
AjayYadavAi <proajayyadav0525@gmail.com>
2020-07-30 19:52:40 +0530

    List added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,17 +8,17 @@
               <div class="row">
                 <div class="col-sm-12">
                   <h1 class="font-weight-600 mb-4">
-                    SPORTS
+                    Today News 
                   </h1>
                 </div>
               </div>
               <div class="row">
-                <div class="col-lg-8">
+                <div class="col-lg-12" v-for="(news,i) in newslist" :key="i">
                   <div class="row">
                     <div class="col-sm-4 grid-margin">
                       <div class="rotate-img">
                         <img
-                          :src="`${url}theme/assets/images/sports/Sports_7.jpg`"
+                          :src="news.urlToImage"
                           alt="banner"
                           class="img-fluid"
                         />
@@ -26,271 +26,13 @@
                     </div>
                     <div class="col-sm-8 grid-margin">
                       <h2 class="font-weight-600 mb-2">
-                        No charges over 2017 battle bus cases
+                        {{ news.title }}
                       </h2>
                       <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
+                        <span class="mr-2">Photo </span>{{ news.publishedAt }}
                       </p>
                       <p class="fs-15">
-                        Lorem Ipsum has been the industry's standard dummy
-                        text ever since the 1500s, when an unknown printer
-                        took a galley of type and scrambled it to make a
-                        type specimen book.
-                      </p>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-4 grid-margin">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_8.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-8 grid-margin">
-                      <h2 class="font-weight-600 mb-2">
-                        No charges over 2017 battle bus cases
-                      </h2>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
-                      </p>
-                      <p class="fs-15">
-                        Lorem Ipsum has been the industry's standard dummy
-                        text ever since the 1500s, when an unknown printer
-                        took a galley of type and scrambled it to make a
-                        type specimen book.
-                      </p>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-4 grid-margin">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_9.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-8 grid-margin">
-                      <h2 class="font-weight-600 mb-2">
-                        Kaine: Trump Jr. may have committed treason
-                      </h2>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
-                      </p>
-                      <p class="fs-15">
-                        Lorem Ipsum has been the industry's standard dummy
-                        text ever since the 1500s, when an unknown printer
-                        took a galley of type and scrambled it to make a
-                        type specimen book.
-                      </p>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-4 grid-margin">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_10.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-8 grid-margin">
-                      <h2 class="font-weight-600 mb-2">
-                        South Korea’s Moon Jae-in sworn in vowing to address
-                      </h2>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
-                      </p>
-                      <p class="fs-15">
-                        Lorem Ipsum has been the industry's standard dummy
-                        text ever since the 1500s, when an unknown printer
-                        took a galley of type and scrambled it to make a
-                        type specimen book.
-                      </p>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-4 grid-margin">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_11.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-8 grid-margin">
-                      <h2 class="font-weight-600 mb-2">
-                        No charges over 2017 battle bus cases
-                      </h2>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
-                      </p>
-                      <p class="fs-15">
-                        Lorem Ipsum has been the industry's standard dummy
-                        text ever since the 1500s, when an unknown printer
-                        took a galley of type and scrambled it to make a
-                        type specimen book.
-                      </p>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-4 grid-margin">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_12.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-8 grid-margin">
-                      <h2 class="font-weight-600 mb-2">
-                        Kaine: Trump Jr. may have committed treason
-                      </h2>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
-                      </p>
-                      <p class="fs-15">
-                        Lorem Ipsum has been the industry's standard dummy
-                        text ever since the 1500s, when an unknown printer
-                        took a galley of type and scrambled it to make a
-                        type specimen book.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-4">
-                  <h2 class="mb-4 text-primary font-weight-600">
-                    Latest news
-                  </h2>
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <div class="border-bottom pb-4 pt-4">
-                        <div class="row">
-                          <div class="col-sm-8">
-                            <h5 class="font-weight-600 mb-1">
-                              Ways to stay social online while in self..
-                            </h5>
-                            <p class="fs-13 text-muted mb-0">
-                              <span class="mr-2">Photo </span>10 Minutes ago
-                            </p>
-                          </div>
-                          <div class="col-sm-4">
-                            <div class="rotate-img">
-                              <img
-                                :src="`${url}theme/assets/images/sports/Sports_1.jpg`"
-                                alt="banner"
-                                class="img-fluid"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <div class="border-bottom pb-4 pt-4">
-                        <div class="row">
-                          <div class="col-sm-8">
-                            <h5 class="font-weight-600 mb-1">
-                              Premier League players join charity..
-                            </h5>
-                            <p class="fs-13 text-muted mb-0">
-                              <span class="mr-2">Photo </span>10 Minutes ago
-                            </p>
-                          </div>
-                          <div class="col-sm-4">
-                            <div class="rotate-img">
-                              <img
-                                :src="`${url}theme/assets/images/sports/Sports_2.jpg`"
-                                alt="banner"
-                                class="img-fluid"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <div class="pt-4">
-                        <div class="row">
-                          <div class="col-sm-8">
-                            <h5 class="font-weight-600 mb-1">
-                              UK Athletics board changed stance on..
-                            </h5>
-                            <p class="fs-13 text-muted mb-0">
-                              <span class="mr-2">Photo </span>10 Minutes ago
-                            </p>
-                          </div>
-                          <div class="col-sm-4">
-                            <div class="rotate-img">
-                              <img
-                                :src="`${url}theme/assets/images/sports/Sports_3.jpg`"
-                                alt="banner"
-                                class="img-fluid"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="trending">
-                    <h2 class="mb-4 text-primary font-weight-600">
-                      Trending
-                    </h2>
-                    <div class="mb-4">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_4.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                      <h3 class="mt-3 font-weight-600">
-                        Virus Kills Member Of Advising Iran’s Supreme
-                      </h3>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
-                      </p>
-                    </div>
-                    <div class="mb-4">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_5.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                      <h3 class="mt-3 font-weight-600">
-                        Virus Kills Member Of Advising Iran’s Supreme
-                      </h3>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
-                      </p>
-                    </div>
-                    <div class="mb-4">
-                      <div class="rotate-img">
-                        <img
-                          :src="`${url}theme/assets/images/sports/Sports_6.jpg`"
-                          alt="banner"
-                          class="img-fluid"
-                        />
-                      </div>
-                      <h3 class="mt-3 font-weight-600">
-                        Virus Kills Member Of Advising Iran’s Supreme
-                      </h3>
-                      <p class="fs-13 text-muted mb-0">
-                        <span class="mr-2">Photo </span>10 Minutes ago
+                        {{ news.description }}
                       </p>
                     </div>
                   </div>
@@ -309,8 +51,21 @@ export default {
 	name: 'home',
 	data(){
 		return {
-			url:process.env.BASE_URL
+			url:process.env.BASE_URL,
+      newslist:[],
 		}
-	}
+	},
+  mounted(){
+    this.latesNews();
+  },
+  methods:{
+    async latesNews(){
+      var api = "https://newsapi.org/v2/top-headlines?country=in&apiKey="+process.env.VUE_APP_API_KEY;
+      const res = await fetch(api);
+      const data = await res.json();
+      this.newslist = data.articles;
+      console.log(this.newslist);
+    },
+  }
 }
 </script>
